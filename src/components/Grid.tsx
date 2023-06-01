be6174c5e84dc018e86bad6186836a80e86aa9fe
Marine Ramillon <marine.ramillon@gmail.com>
2023-06-01 22:01:08 +0200

    display shows per channel

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -19,8 +19,26 @@ class Component extends React.Component<propsType> {
     return this.padStr(dte.getDate()) + '/' + this.padStr(dte.getMonth() + 1) + '/' + dte.getFullYear();
   }
 
+  dateToHoursAndMinutes(input) {
+    const hours = Math.floor(+input / 3600); // Conversion en heures
+    const minutes = Math.floor((+input % 3600) / 60); // Conversion en minutes
+    const formattedHours = (hours < 10) ? "0" + hours : hours;
+    const formattedMinutes = (minutes < 10) ? "0" + minutes : minutes;
+
+    return formattedHours + ":" + formattedMinutes;
+  }
+
+  getDurationPourcentage(startTime, endTime) {
+    // 97200 correspond à l'horaire de fin des programmes de la journée.
+    console.log(((endTime - startTime) / (97200 -10800)) * 100 *50)
+    return ((endTime - startTime) / (97200 -10800)) * 100
+  }
+
+
+
   render() {
     const { data } = this.props;
+
 
     return (
       <div className="Grid">
@@ -28,14 +46,33 @@ class Component extends React.Component<propsType> {
           {'Grille du ' + this.dateToDdMmYyyy(data.day)}
         </div>
         <div className="Chns">
-          {data.chns.map(chn =>
+          {data.chns.map(chn => (
             <div key={chn.key} className="Chn">
-              {chn.key}
+              <div className="ChnKey">{chn.key}</div>
+              <div className="Shows">
+                {chn.shows.map(show => (
+                  <div
+                    key={show.startTime}
+                    className="Show"
+                    style={{ height: `${this.getDurationPourcentage(show.startTime, show.endTime) * 50}px` }}
+                  >
+                    <div className="ShowMainInfos">
+                      <div className="ShowHoursAndPDA">
+                        <div className="ShowTime">{this.dateToHoursAndMinutes(show.startTime)}</div>
+                        <div>PDA : {show.pda}</div>
+                      </div>
+                    <div className="ShowTitle">{show.title}</div>
+                    <div>{this.getDurationPourcentage(show.startTime, show.endTime).toFixed(2)}%</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     );
+
   }
 }
 
